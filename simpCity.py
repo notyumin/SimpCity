888ae447da95d81f653f5e8b2113c79ed8caa626
jolenecpn <jolene.cheongg@gmail.com>
2021-12-09 00:20:47 +0800

    implementation of in-game menu (issue #4, #7) and logic for saving and loading board data (issue #5, #6)

--- simpCity.py
+++ simpCity.py
@@ -1,6 +1,4 @@
-# initial commit
-print("Welcome, mayor of Simp City")
-print("----------------------------")
+import pickle
 
 
 def init_game():
@@ -21,27 +19,80 @@ def init_game():
     }
     return game_board, building_pool
 
-
+#UI for in-game menu
 def game_menu(game_board, building_pool):
-    print("This is the game menu...")
-    return
+    #implement auto Turn counter
+    print("\nTurn Counter")
+
+    #implement display game board 
+    print("Board")
+
+    #in-game menu
+    print("1. Build random building") 
+    print("2. Build random building")
+    print("3. See remaining buildings")
+    print("4. See current score")
+    print("\n5. Save game")
+    print("0. Exit to main menu")
+    try:
+        choice = int(input("Your choice? "))
+        if choice == 1:
+            #code to add random building 1
+            return game_menu(game_board, building_pool)
+        elif choice == 2:
+            #code to add random building 2
+            return game_menu(game_board, building_pool)
+        elif choice == 3:
+            #code to see remaining buildings
+            return game_menu(game_board, building_pool)
+        elif choice == 4:
+            #code to see current score
+            return game_menu(game_board, building_pool)
+        elif choice == 5:
+            save_game()
+            return game_menu(game_board, building_pool)
+        elif choice == 0:
+            #code to delete existing game data 
+            return
+        else:
+            print("\033[91m{}\033[00m".format("Input options 0-5!"))
+            return game_menu(game_board, building_pool) 
+    except ValueError:
+            print("\033[91m{}\033[00m".format("Input options 0-5!"))
+            return game_menu(game_board, building_pool)
 
 
+# Function to save game data
+def save_game():
+    #getting board data (implement get board function)
+    board = {1:"6",2:"5",3:"f"} #to change to game data, populating for function testing purpose
+    pickle_out = open("save.pickle", "wb")
+    pickle.dump(board, pickle_out)
+    pickle_out.close()
+
+# Function to load game data
 def load_game():
-    # implementation
+    pickle_in = open("save.pickle", "rb")
+    board = pickle.load(pickle_in)
+    #implement an update function to populate saved board
+    # board = city.updatefunction
+    
+    #testing whether function works 
+    print(board)
     return
-
 
 def main():
     game_board = None
     building_pool = None
 
     while True:
+        print("\nWelcome, mayor of Simp City!")
+        print("----------------------------")
         print("\n1. Start new game")
         print("2. Load new game")
         print("\n0. Exit")
 
-        option = input("Your choice? :")
+        option = input("Your choice? ")
 
         # Ensure inputted option is valid
         try:
