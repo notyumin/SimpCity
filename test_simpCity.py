067946bae01764afbeb7c6a3eba2a02b5b16c587
Arjun <harjun751@gmail.com>
2021-12-11 17:31:26 +0800

    Updated randomise_building unit test to check for 2 buildings. Added more cases.

--- test_simpCity.py
+++ test_simpCity.py
@@ -15,12 +15,17 @@ def test_init_game_creates_game_board_and_pool():
 @pytest.mark.parametrize("pool,  expected_buildings", [
     ({"HSE": 8, "FAC": 8, "SHP": 8, "HWY": 8, "BCH": 8},
      ["HSE", "FAC", "SHP", "HWY", "BCH"]),
-    ({"HSE": 0, "FAC": 0, "SHP": 0,
-      "HWY": 0, "BCH": 8}, ["BCH"])
+    ({"HSE": 0, "FAC": 0, "SHP": 0, "HWY": 0, "BCH": 8},
+     ["BCH"]),
+    ({"HSE": 0, "FAC": 0, "SHP": 0, "HWY": 0, "BCH": 1},
+     ["BCH", None]),
+    ({"HSE": 0, "FAC": 0, "SHP": 0, "HWY": 0, "BCH": 0},
+     [None])
 ])
 def test_randomise_buildings_returns_valid_building(pool, expected_buildings):
     # Act
-    building = randomise_building(pool)
+    buildings = randomise_building(pool)
 
     # Assert
-    assert building in expected_buildings
+    assert buildings[0] in expected_buildings
+    assert buildings[1] in expected_buildings
