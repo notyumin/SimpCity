10f9b49a0d0a82d890d275538fa1d4e2afa0c821
jolenecpn <jolene.cheongg@gmail.com>
2022-01-12 21:35:42 +0800

    finally fixed unit test

--- test_simpCity.py
+++ test_simpCity.py
@@ -21,26 +21,26 @@ def test_save_and_load_game(board, pool, expectedBoard, expectedPool):
     assert eBoard == expectedBoard
     assert ePool ==  expectedPool
 
-#load test         doesnt work need help due to fs 
-# @pytest.mark.parametrize("board, pool, expectedBoard, expectedPool", [([['', '', '', ''], ['', '', '', '']], {'HSE': 8, "FAC": 8, "SHP": 8, "HWY": 8, "BCH": 8},  
-#                                                         [['', '', '', ''], ['', '', '', '']], {'HSE': 8, "FAC": 8, "SHP": 8, "HWY": 8, "BCH": 8})])
-# def test_load_game(fs):
-#     board = [['HSE', '', '', ''], ['', '', '', ''],
-#                           ['', '', '', ''], ['', '', '', ''], ]
-#     pool = {"HSE": 8, "FAC": 8, "SHP": 8, "HWY": 8, "BCH": 8}
-#     pickle_out = open(fs.create_file('load_test.pickle'), "wb")
-#     pickle.dump([board, pool], pickle_out)
-#     eBoard,ePool = load_game()
-#     assert eBoard == board
-#     assert ePool ==  pool
+def create_fake_file(fs):
+    fs.create_file('load_test.pickle')
 
-#saving test        doesnt work need help due to fs
-# @pytest.mark.parametrize("board, pool, expectedBoard, expectedPool", [([['', '', '', ''], ['', '', '', '']], {'HSE': 8, "FAC": 8, "SHP": 8, "HWY": 8, "BCH": 8},  
-#                                                         [['', '', '', ''], ['', '', '', '']], {'HSE': 8, "FAC": 8, "SHP": 8, "HWY": 8, "BCH": 8})])
-# def test_save_game(board, pool, expectedBoard, expectedPool,fs):
-#     filename = fs.create_file('save_test.pickle')
-#     save_game (board, pool, filename)
-#     pickle_in = open(filename, "rb")
-#     sBoard = pickle.load(pickle_in)
-#     assert sBoard[0] == expectedBoard
-#     assert sBoard[1] ==  expectedPool
+#load test         
+@pytest.mark.parametrize("board, pool, expectedBoard, expectedPool", [([['', '', '', ''], ['', '', '', '']], {'HSE': 8, "FAC": 8, "SHP": 8, "HWY": 8, "BCH": 8},  
+                                                       [['', '', '', ''], ['', '', '', '']], {'HSE': 8, "FAC": 8, "SHP": 8, "HWY": 8, "BCH": 8})])
+def test_load_game(fs, board, pool, expectedBoard, expectedPool):
+    pickle_out = open('load_test.pickle', "wb")
+    pickle.dump([board, pool], pickle_out)
+    pickle_out.close()
+    eBoard,ePool = load_game('load_test.pickle')
+    assert eBoard == expectedBoard
+    assert ePool ==  expectedPool
+
+# saving test        
+@pytest.mark.parametrize("board, pool, expectedBoard, expectedPool", [([['', '', '', ''], ['', '', '', '']], {'HSE': 8, "FAC": 8, "SHP": 8, "HWY": 8, "BCH": 8},  
+                                                         [['', '', '', ''], ['', '', '', '']], {'HSE': 8, "FAC": 8, "SHP": 8, "HWY": 8, "BCH": 8})])
+def test_save_game(board, pool, expectedBoard, expectedPool,fs):
+    save_game (board, pool, 'save_test.pickle')
+    pickle_in = open('save_test.pickle', "rb")
+    sBoard = pickle.load(pickle_in)
+    assert sBoard[0] == expectedBoard
+    assert sBoard[1] ==  expectedPool
