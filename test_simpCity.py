e07cc3e286efa21a25ce0a5d491ef5ffaf8a4cfa
jolenecpn <jolene.cheongg@gmail.com>
2022-01-12 21:37:20 +0800

    delete previous test code

--- test_simpCity.py
+++ test_simpCity.py
@@ -12,18 +12,6 @@ def test_init_game_creates_game_board_and_pool():
                           ['', '', '', ''], ['', '', '', ''], ]
     assert building_pool == {"HSE": 8, "FAC": 8, "SHP": 8, "HWY": 8, "BCH": 8}
 
-
-@pytest.mark.parametrize("board, pool, expectedBoard, expectedPool", [([['', '', '', ''], ['', '', '', '']], {'HSE': 8, "FAC": 8, "SHP": 8, "HWY": 8, "BCH": 8},  
-                                                        [['', '', '', ''], ['', '', '', '']], {'HSE': 8, "FAC": 8, "SHP": 8, "HWY": 8, "BCH": 8})])
-def test_save_and_load_game(board, pool, expectedBoard, expectedPool):
-    save_game (board, pool)
-    eBoard,ePool = load_game()
-    assert eBoard == expectedBoard
-    assert ePool ==  expectedPool
-
-def create_fake_file(fs):
-    fs.create_file('load_test.pickle')
-
 #load test         
 @pytest.mark.parametrize("board, pool, expectedBoard, expectedPool", [([['', '', '', ''], ['', '', '', '']], {'HSE': 8, "FAC": 8, "SHP": 8, "HWY": 8, "BCH": 8},  
                                                        [['', '', '', ''], ['', '', '', '']], {'HSE': 8, "FAC": 8, "SHP": 8, "HWY": 8, "BCH": 8})])
