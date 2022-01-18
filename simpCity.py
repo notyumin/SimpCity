0e2f17bf471b63f91ad242ebbfc4800c4f58990f
Arjun <harjun751@gmail.com>
2022-01-18 09:38:39 +0800

    Added comments  & rearranged function

--- simpCity.py
+++ simpCity.py
@@ -183,44 +183,6 @@ def build(board, column, row, building):
 
     return board
 
-
-def main():
-    game_board = None
-    building_pool = None
-
-    while True:
-        print("\nWelcome, mayor of Simp City!")
-        print("----------------------------")
-        print("\n1. Start new game")
-        print("2. Load new game")
-        print("\n0. Exit")
-
-        option = input("Your choice? ")
-
-        # Ensure inputted option is valid
-        try:
-            option = int(option)
-            if option != 1 and option != 2 and option != 0:
-                raise ValueError
-        except ValueError:
-            # print red warning using ANSI escape codes
-            print("\033[91m{}\033[00m".format("Invalid option!"))
-            continue
-
-        if option == 1:
-            game_board, building_pool = init_game()
-            # Start game menu
-            game_menu(game_board, building_pool)
-
-        elif option == 2:
-            game_board, building_pool = load_game("save.pickle")
-            game_menu(game_board, building_pool)
-
-        elif option == 0:
-            print("Bye!")
-            exit()
-
-
 def calculate_score(game_board):
     factory_count = 0
     monument_count = 0
@@ -256,6 +218,7 @@ def calculate_score(game_board):
                 factory_count += 1
 
             elif curr_item == "HSE":
+                # HSE is scored based on items around it
                 item_above, item_below, item_right, item_left = get_items_around(
                     game_board, i, y
                 )
@@ -272,14 +235,17 @@ def calculate_score(game_board):
                 HSE_scores.append(sub_total)
 
             elif curr_item == "SHP":
+                # SHP is scored based on unique items around it
                 item_above, item_below, item_right, item_left = get_items_around(
                     game_board, i, y
                 )
                 sub_total = 0
+                # Checks the items around
                 unique_buildings = ["BCH", "FAC", "HSE", "HWY", "MON", "PRK"]
                 for item in [item_above, item_below, item_right, item_left]:
                     if item in unique_buildings:
                         sub_total += 1
+                        # remove unique building from pool so that it can't be scored again
                         unique_buildings.remove(item)
                 SHP_scores.append(sub_total)
 
@@ -287,6 +253,8 @@ def calculate_score(game_board):
                 x = 0
                 # Each individual highway gets scored based on how long the highway is
                 length = 1
+
+                # Checks right & left of current item and adds to length of highway
                 while True:
                     item_right = get_items_around(game_board, i, y + x)[2]
                     x += 1
@@ -306,6 +274,7 @@ def calculate_score(game_board):
 
             elif curr_item == "PRK":
                 current_park = crawl_parks(game_board, i, y, [])
+                # Park is scored based on the size. Crosscheck with score dictionary. Max size of scoring for a park is 8
                 PRK_scores.append(PRK_score_dict[min(len(current_park), 8)])
                 # Add park to coordinate blacklist - don't need to traverse again
                 coordinate_blacklist.extend(current_park)
@@ -328,14 +297,16 @@ def calculate_score(game_board):
                 # For an item to  be in a corner, exactly 2 of the checks must eval to true
                 if corner_checks.count(True) == 2:
                     monument_corner_count += 1
+                    # Append corner monument score
                     MON_scores.append(2)
                 else:
+                    # Append normal monument score
                     MON_scores.append(1)
 
-    # FAC scores 1pt/FAC up to  a maximum of 4pts/FAC.
+    # FAC scores 1pt/FAC up to  a maximum of 4pts/FAC. Caps out at 4 FACs, then 1pt/FAC
     FAC_scores = [min(factory_count, 4) if i<4 else 1 for i in range(factory_count)]
 
-    # MON
+    # Monuments get scored normally unless there are 3 corner-built monuments
     if monument_corner_count >= 3:
         # Override monument score
         MON_scores = [4 for i in range(monument_count)]
@@ -381,12 +352,15 @@ def calculate_score(game_board):
     print("\nTotal score: " + str(total_score))
     return total_score
 
-
+# Obtains coordinates of any chunk of vertically & horizontally connected PRK items
 def crawl_parks(game_board, i, y, park_coords):
     item_above, item_below, item_right, item_left = get_items_around(game_board, i, y)
     coords = (i, y)
+    # append current coordinates to park coords
     park_coords.append(coords)
 
+    # Check according to ↓, →, ↑, ←
+    # Traversed items are stored in park_coords
     if item_below == "PRK" and (i + 1, y) not in park_coords:
         park_coords = crawl_parks(game_board, i + 1, y, park_coords)
 
@@ -404,6 +378,8 @@ def crawl_parks(game_board, i, y, park_coords):
 
 
 def get_items_around(game_board, i, y):
+    # obtains items around a given coordinate.
+    # Returns none for an item if it's out-of-bounds
     try:
         if (i - 1) >= 0:
             item_above = game_board[i - 1][y]
@@ -428,6 +404,42 @@ def get_items_around(game_board, i, y):
         item_left = None
     return item_above, item_below, item_right, item_left
 
+
+def main():
+    game_board = None
+    building_pool = None
+
+    while True:
+        print("\nWelcome, mayor of Simp City!")
+        print("----------------------------")
+        print("\n1. Start new game")
+        print("2. Load new game")
+        print("\n0. Exit")
+
+        option = input("Your choice? ")
+
+        # Ensure inputted option is valid
+        try:
+            option = int(option)
+            if option != 1 and option != 2 and option != 0:
+                raise ValueError
+        except ValueError:
+            # print red warning using ANSI escape codes
+            print("\033[91m{}\033[00m".format("Invalid option!"))
+            continue
+
+        if option == 1:
+            game_board, building_pool = init_game()
+            # Start game menu
+            game_menu(game_board, building_pool)
+
+        elif option == 2:
+            game_board, building_pool = load_game("save.pickle")
+            game_menu(game_board, building_pool)
+
+        elif option == 0:
+            print("Bye!")
+            exit()
 
 if __name__ == "__main__":
     main()
